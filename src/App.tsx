a4ece0e601fe9048e99eda68d54608c19cf49a01
guithr <guilherme.thrmatos@gmail.com>
2025-02-18 12:22:34 -0300

    feat: finalize Guess Game with full functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,138 @@
 import styles from "./app.module.css";
+import { useEffect, useState } from "react";
+import { WORDS, Challenge } from "./utils/words";
 
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { LettersUsed, LettersUsedProps } from "./components/LettersUsed";
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+  const ATTEMPTS_MARGIN = 5;
+
   function handleRestartGame() {
-    alert("reiniciar o jogo");
+    const isConfirmed = window.confirm("Você tem certeza que deseja reinciar?");
+    if (isConfirmed) {
+      startGame();
+    }
   }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+
+    setScore(0);
+    setLetter("");
+    setLettersUsed([]);
+  }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra");
+    }
+
+    const value = letter.toUpperCase();
+    const exists = lettersUsed.find(
+      (used) => used.value.toUpperCase() === value
+    );
+
+    if (exists) {
+      setLetter("");
+      return alert("Letra já utilizada");
+    }
+
+    const hits = challenge.word
+      .toUpperCase()
+      .split("")
+      .filter((char) => char === value).length;
+
+    const correct = hits > 0;
+    const currentScore = score + hits;
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }]);
+    setScore(currentScore);
+
+    setLetter("");
+  }
+
+  function endGame(message: string) {
+    alert(message);
+    startGame();
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  useEffect(() => {}, [score, lettersUsed.length]);
+
+  setTimeout(() => {
+    if (score === challenge?.word.length) {
+      return endGame("Parabéns você descobriu a palavra!");
+    }
+
+    const attemptLimit = challenge?.word.length + ATTEMPTS_MARGIN;
+
+    if (lettersUsed.length === attemptLimit) {
+      return endGame("Você excedeu o limite de tentativas!");
+    }
+  }, 300);
+
+  if (!challenge) {
+    return;
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
-        <Tip tip="Uma das linguagens mais utilizadas" />
+        <Header
+          current={lettersUsed.length}
+          max={challenge.word.length + ATTEMPTS_MARGIN}
+          onRestart={handleRestartGame}
+        />
+
+        <Tip tip={challenge.tip} />
+
         <div className={styles.word}>
-          <Letter value="r" />
-          <Letter value="e" />
-          <Letter value="a" />
-          <Letter value="c" />
-          <Letter value="t" />
+          {challenge.word.split("").map((letter, index) => {
+            const letterUsed = lettersUsed.find(
+              (used) => used.value.toUpperCase() === letter.toUpperCase()
+            );
+
+            return (
+              <Letter
+                key={index}
+                value={letterUsed?.value}
+                color={letterUsed?.correct ? "correct" : "default"}
+              />
+            );
+          })}
         </div>
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+          <Button title="Confirmar" onClick={handleConfirm} />
+        </div>
+
+        <LettersUsed data={lettersUsed} />
       </main>
     </div>
   );
